3deade04af42f7f4a9baeb234709a5ca1526b74d
oliviabruvik <oliviabruvik@gmail.com>
2021-07-27 21:50:39 +0000

    add day 12 functionality

---
+++ day12/starter/firebaseNotes/public/js/viewNotes.js
@@ -0,0 +1,85 @@
+let userName;
+
+window.onload = () => {
+    // When page loads, check user logged in state
+    firebase.auth().onAuthStateChanged((user) => {
+        
+        if (user) {
+            //  If logged in, get user's notes from db
+            const googleUserId = user.uid;
+            userName = user.displayName;
+            console.log("UN: ", userName)
+            getNotes(googleUserId);
+    
+        } else {
+            // If not logged in redirect to log in page
+            window.location = 'index.html';
+        }
+
+    })
+}
+
+// Get notes from db, display notes on page
+const getNotes = (userId) => {
+    console.log(userId);
+    const userRef = firebase.database().ref(`users/${userId}`);
+    userRef.on('value', snapshot => {
+        const data = snapshot.val();
+        writeNotesToHtml(data);
+    })
+};
+
+const writeNotesToHtml = (data) => {
+    // Put all html into page at once
+    const labelDropdown = document.querySelector("#labels-dropdown");
+    const noteRenderArea = document.querySelector('#app');
+    for (let noteKey in data) {
+        // Create html string for one note
+        let noteHtml = createHtmlForNote(data[noteKey]);
+        console.log("data: ", data);
+        console.log("data[noteKey]: ", data[noteKey]);
+        noteRenderArea.innerHTML += noteHtml;
+
+        let labelHtml = addLabelDropdown(data[noteKey]);
+        labelDropdown.innerHTML += labelHtml;
+    }
+};
+
+
+const filterOnLabel = () => {
+    document.querySelector(`card-${label}`)
+};
+// .remove() + condition
+// Remove all elements that don't have matching labels
+
+const addLabelDropdown = (note) => {
+    console.log("SUCCESS");
+    return `<a onclick="filterOnLabel()" class="dropdown-item">
+                ${note.label}
+            </a>`;
+};
+
+
+
+const createHtmlForNote = (note) => {
+    console.log(note);
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    //TODO: create html and put in the note data
+    return `<div class="column is-one-third">
+                <div class="card" style="background-color: #${randomColor}" id="card-${note.label}">
+                    <header class="card-header">
+                        <p class="card-header-title" style="color: white">
+                            ${note.title}
+                        </p>
+                        <p class="subtitle is-6" style="color: white">
+                            ${userName}
+                        </p>
+                    </header>
+                    <div class="card-content " style="color: white">
+                        <div class="content">
+                            ${note.note}
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+};
